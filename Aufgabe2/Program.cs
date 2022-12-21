06cf9b70afc8350b9ce1b11e440c98caf34ef8b8
Nata Schreiner <natashreiner@gmail.com>
2022-12-21 19:21:48 +0700

    Task 2 done

---
+++ Aufgabe2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (numberA > numberB)
+{
+    max = numberA;
+}
+else 
+{
+    max = numberB;
+}
+
+Console.WriteLine(max);
