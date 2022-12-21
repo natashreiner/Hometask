c75c2ac3a27eda5a5debdd3f79f1458dd8619e6f
Nata Schreiner <natashreiner@gmail.com>
2022-12-21 21:54:21 +0700

    task 6 is done

---
+++ Aufgabe6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+
+int div = numberA%2;
+
+
+if (div == 1)
+{
+    Console.WriteLine("нечетное");
+}
+else
+{
+    Console.WriteLine("четное");
+}
