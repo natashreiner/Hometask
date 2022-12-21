efb044f17d5383685938012fb2225aae9d1108ef
Nata Schreiner <natashreiner@gmail.com>
2022-12-21 19:36:03 +0700

    task 4 done

---
+++ Aufgabe4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число C: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
