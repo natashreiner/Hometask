56df11f3b68897640e973f347713687f0db678d1
Nata Schreiner <natashreiner@gmail.com>
2022-12-21 20:46:47 +0700

    task 8 done

---
+++ Aufgabe8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+int count = 2;
+
+while (count <= numberN)
+{
+    Console.Write($"{count}, ");
+    count = count + 2;
+}
