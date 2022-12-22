6abf6459e03c059062c03e7ee3d0173d08072930
Nata Schreiner <natashreiner@gmail.com>
2022-12-23 01:15:51 +0700

    task 15 done

---
+++ Aufgabe15/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+
+string s = "не день недели";
+
+if (day == 1)
+{
+    s = "рабочий день";
+}
+else if (day == 2)
+{
+    s = "рабочий день";
+}
+else if (day == 3)
+{
+    s = "рабочий день";
+}
+else if (day == 4)
+{
+    s = "рабочий день";
+}
+else if (day == 5)
+{
+    s = "рабочий день";
+}
+else if (day == 6)
+{
+    s = "выходной день";
+}
+else if (day == 7)
+{
+    s = "выходной день";
+}
+System.Console.WriteLine(s);
