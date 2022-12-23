1d04ed05b75bddf7a1fb0d2b3698f523957487df
Nata Schreiner <natashreiner@gmail.com>
2022-12-24 01:41:49 +0700

    task 13 done

---
+++ Aufgabe13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите число");
+int N = int.Parse(Console.ReadLine());
+int b = 0;
+if(N > 100)
+{
+    while(N > 1000)
+    {
+        N = N / 10;
+    }
+    Console.WriteLine($"Третье число {b = N % 10}");
+}
+else Console.WriteLine("Третьего числа нет");
+
+
+
