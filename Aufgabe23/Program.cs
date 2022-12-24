9af745fe11be59bd95622b9d4e44b5fea02e9e42
Nata Schreiner <natashreiner@gmail.com>
2022-12-25 00:47:26 +0700

    task 23 done

---
+++ Aufgabe23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int count = 1;
+while(count <= N)
+{
+    Console.Write($"{Math.Pow(count, 3)}-//-");
+    count++;
+}
