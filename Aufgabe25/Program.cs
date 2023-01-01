83cee2bf813f52a4a7739237c42df944b79b5fc0
Nata Schreiner <natashreiner@gmail.com>
2023-01-02 02:15:55 +0700

    task 25 copied but doen't work

---
+++ Aufgabe25/Program.cs
@@ -0,0 +1,46 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+void Main()
+{
+    bool isWorking = true;
+    while (isWorking)
+    {
+        Console.Write("Input command: ");
+        switch (Console.ReadLine())
+        {
+            case "Task25": Task25(); break;
+            case "exit": isWorking = false; break;
+        }
+    }
+}
+
+void Task25()
+{
+    Console.WriteLine("Task 25");
+
+    int numbA = ReadInt("First number (A)");
+    int numbB = ReadInt("Second number (B)");
+
+    Console.WriteLine($"Степень {numbB} от числа {numbA} равна {Pow(numbA, numbB)}");
+}
+
+int ReadInt(string argumentName)
+{
+    Console.Write($"Input {argumentName}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int Pow(int firstNumber, int secondNumber)
+{
+    int result = 1;
+    for (int i = 0; i < secondNumber; i++)
+    {
+        result *= firstNumber;
+    }
+    return result;
+}
+
+//int ReadInt()
+//{
+  //  return int.Parse(Console.ReadLine());
+//}
