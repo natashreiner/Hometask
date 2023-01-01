42c41e15ab5daff4409032ed7afdc15bbab1679b
Nata Schreiner <natashreiner@gmail.com>
2023-01-02 01:06:11 +0700

    tried another way, unsuccsessfully

--- Aufgabe29/Program.cs
+++ Aufgabe29/Program.cs
@@ -1,4 +1,6 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//мое решение, по аналогии с лекцией
+
 void PrintArray(int[] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -21,3 +23,57 @@ PrintArray(arr);
 FillArray(arr);
 Console.WriteLine();
 PrintArray(arr);
+
+// как было на семинаре, докопалась до аргументов метода ReadInt, не смогла запустить, хоть убейся
+
+/*void Main()
+{
+    bool isWorking = true;
+    while (isWorking)
+    {
+        Console.Write("Input command: ");
+        switch (Console.ReadLine())
+        {
+            case "Task29": Task29(); break;
+            case "exit": isWorking = false; break;
+        }
+    }
+}
+
+void Task29()
+{
+    Console.WriteLine("Task29");
+
+    int[] array = GetArray(ReadInt("array length"));
+
+    Console.WriteLine(ArrayToString(array));
+}
+
+int ReadInt()
+{
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GetArray(int length)
+{
+    int[] array = new int [length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInt($"elemnt {i}");
+    }
+    return array;
+}
+
+string ArrayToString(int[] array)
+{
+    string result = string.Empty;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i]}, ";
+    }
+    return result;
+}*/
+
+
+
