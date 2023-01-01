d8cefa316e96b9eb3f45f75e2cc3bb8dce48821a
Nata Schreiner <natashreiner@gmail.com>
2023-01-02 04:18:40 +0700

    task 27 done but without array

---
+++ Aufgabe27/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Input number");
+int n = ReadInt("Number");
+int sum = 0;
+while (n > 0)
+{
+    sum += n % 10;
+    n = n/10;
+}
+Console.WriteLine(sum);
+
+
+int ReadInt(string argumentName)
+{
+	Console.Write($"Input {argumentName}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+//string number = ReadInt("number").ToString();
+//void PrintArray(int[] number)
+//{
+  //  int count = number.Length;
+    //for (int i = 0; i < count; i++)
+    //{
+      //  Console.Write($"{number[i]}");
+    //}    
+    //Console.WriteLine();
+//}
+
+/*int ArrayToString(int[] number)
+{
+    string ArrayToString = string.Empty;
+    int sum = 0;
+    for(int i = 0; i < number.Length; i++)
+    {
+        sum = sum + number[i];
+    }
+    return sum;
+}*?
+
+
+
+//Console.WriteLine(sum);
+
+/*string ArrayToString(int[] array)
+{
+    int sum = 0;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+        sum = sum + array[i];
+	}
+	return sum;
+    Console.WriteLine(sum);
+}*/
+
+
+
