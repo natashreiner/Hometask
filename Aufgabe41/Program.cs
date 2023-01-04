0d897357fb369416a53bd33a7d4c1dee04c1be7e
Nata Schreiner <natashreiner@gmail.com>
2023-01-05 01:36:47 +0700

    task 41 done

---
+++ Aufgabe41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41 Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+int[] array = GetArray(ReadInt("array length"));
+PrintArray(array);
+GetPositivNumbers(array);
+
+int ReadInt(string argumentName)
+{
+	Console.Write($"Input {argumentName}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+int[] GetArray(int length)
+{
+	int[] array = new int[length];
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = ReadInt($"element {i}");
+	}
+
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}, ");
+	}
+}
+
+void GetPositivNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count ++;
+        }  
+    }
+    Console.WriteLine($"Вы ввели {count} чисел больше нуля");
+}
