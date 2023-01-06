e40485b01e2308ec963e2e0eab3a01d8f788be51
Nata Schreiner <natashreiner@gmail.com>
2023-01-07 00:34:17 +0700

    task 52 only got array

---
+++ Aufgabe52/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int length = ReadInt("number M");
+int secondLength = ReadInt("number N");
+int [,] array = GetRandomArray(length, secondLength);
+
+PrintArray(array);
+GetAverageInColumn(array);
+
+int ReadInt(string argumentName)
+{
+    Console.Write($"Input {argumentName}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int [,] GetRandomArray(int length, int secondLength)
+{
+    int [,] array = new int [length, secondLength];
+    Random random = new Random();
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = random.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double GetAverageInColumn(int[,] array)
+{
+    double sum = 0;
+    double average = 0;
+   
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        average = sum / (array.GetLength(1) + 1);
+    }
+    return average;
+}
+
