c6387f6948df0442b1aea8db58d85cd308cf6fcf
Nata Schreiner <natashreiner@gmail.com>
2023-01-07 23:58:59 +0700

    task 66 done

---
+++ Aufgabe66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N
+
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+int SumFromTo(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;
+    else if (N == 0) return (M * (M + 1)) / 2;
+    else if (M == N) return M;
+    else if (M < N) return N + SumFromTo(M, N - 1);
+    else return N + SumFromTo(M, N + 1);
+}
+
+Console.WriteLine(SumFromTo(M, N));
+
