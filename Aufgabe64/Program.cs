0f1e5299923ba8718aa66ba4172935d880a676c8
Nata Schreiner <natashreiner@gmail.com>
2023-01-07 21:16:19 +0700

    task 64 done

---
+++ Aufgabe64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void PrintNumbersFromN(int N, int M) 
+{
+    if (N <= M)
+    {
+        Console.WriteLine(N);
+        return;
+    }
+    Console.Write($"{N}, ");
+    PrintNumbersFromN(N - 1, M);
+}
+PrintNumbersFromN(12, 1);
